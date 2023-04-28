d2b9b3f0fc145c1f0395484435e8d5f599753876
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-04-28 11:22:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using SOLID_Principles;
+using SOLID_Principles.DependencyInversion;
+using SOLID_Principles.InterfaceSegregation;
+using SOLID_Principles.Liskov;
+using SOLID_Principles.OpenClosed;
+using SOLID_Principles.SingleResponsibility;
+
+
+namespace Solid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SOLID Principles:");
+
+            var principles = new List<IPrinciple>()
+            {
+                new Srp(),
+                new Ocp(),
+                new Lsp(),
+                new Isp(),
+                new Dip()
+            };
+            principles.ForEach(type =>
+            {
+                Console.WriteLine($"- {type.Principle()}");
+            });
+            Console.Read();
+        }
+    }
+}
